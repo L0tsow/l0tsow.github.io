81a47cc9d53b27a91257cee7ecf4d33f4e3f0ccf
L0tsow <85215760+L0tsow@users.noreply.github.com>
2023-08-08 17:15:04 +0200

    Add files via upload

---
+++ linux.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("u");
+      hiddenText.style.display = "inline"; // Affiche le texte
+    }, 0); // 10000 millisecondes = 10 secondes
+  });
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("u1");
+      hiddenText.style.display = "flex"; // Affiche le texte
+    }, 0); // 10000 millisecondes = 10 secondes
+  });
+
+// 1er commane (cd list)
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("u_r");
+      hiddenText.classList.add("line-1");
+      hiddenText.classList.add("anim-typewriter-ls")
+      hiddenText.style.display = "inline"; // Ajoute la classe d'animation
+    }, 0); // 10000 millisecondes = 10 secondes
+  });
+
+// CD LIST
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("cd_list");
+      hiddenText.style.display = "block"; // Affiche le texte
+    }, 2500); // 10000 millisecondes = 10 secondes
+  });
+
+
+
+
+
+
+
+
+
+
+// 2nd user root@machine
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var aa = document.getElementById("u2");
+      aa.style.opacity = "100%"; // Affiche le texte
+    }, 3500); // 10000 millisecondes = 10 secondes
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var aa = document.getElementById("u3");
+      aa.style.opacity = "100%"; // Affiche le texte
+    }, 3500); // 10000 millisecondes = 10 secondes
+  });
+
+// user request (cd website/)
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("u_r1");
+      hiddenText.classList.add("line-1");
+      hiddenText.classList.add("anim-typewriter-website")
+      hiddenText.style.display = "inline"; // Ajoute la classe d'animation
+    }, 4000); // 10000 millisecondes = 10 secondes
+  });
+  
+
+  console.log("test")
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var aa = document.getElementById("u4");
+      aa.style.opacity = "100%"; // Affiche le texte
+    }, 6500); // 10000 millisecondes = 10 secondes
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var bb = document.getElementById("u5");
+      bb.style.opacity = "100%"; // Affiche le texte
+    }, 6500); // 10000 millisecondes = 10 secondes
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var hiddenText = document.getElementById("u_r2");
+      hiddenText.classList.add("line-1");
+      hiddenText.classList.add("anim-typewriter-website")
+      hiddenText.style.display = "inline"; // Ajoute la classe d'animation
+    }, 7000); // 10000 millisecondes = 10 secondes
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var cc = document.getElementById("load");
+      cc.style.opacity = "100%"; // Affiche le texte
+    }, 10500); // 10000 millisecondes = 10 secondes
+    setTimeout(function(){
+      window.location.href = 'index.html';
+    },11000)
+  });
+
